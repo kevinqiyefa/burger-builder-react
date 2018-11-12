139a4aa8e56eeccee0df9086a00ac2e37b69d04b
Kevin <kevinqiyf@hotmail.com>
2018-11-12 10:38:56 -0800

    updated favicon and added logo component

--- src/components/Navigation/Toolbar/index.js
+++ src/components/Navigation/Toolbar/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Logo from '../Logo';
 
 import './style.css';
 
 const toolbar = props => (
   <header className="Toolbar">
     <div>Menu</div>
-    <div>LOGO</div>
+    <div className="LogoSection">
+      <Logo />
+    </div>
     <nav>...</nav>
   </header>
 );
