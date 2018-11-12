d1619b2d6e3ed21afa1ffc8afc36ff50b7924a4e
Kevin <kevinqiyf@hotmail.com>
2018-11-12 14:22:55 -0800

    added sideDrawer

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,10 +2,12 @@ import Aux from '../../hoc/Aux';
 import React from 'react';
 import './style.css';
 import Toolbar from '../Navigation/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer';
 
 const layout = props => (
   <Aux>
     <Toolbar />
+    <SideDrawer />
     <main className="Content">{props.children}</main>
   </Aux>
 );
---
+++ src/components/Navigation/SideDrawer/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import Logo from '../Logo';
+import NavigationItems from '../NavigationItems';
+import './style.css';
+import Backdrop from '../../UI/Backdrop';
+import Aux from '../../../hoc/Aux';
+
+const sideDrawer = props => {
+  let attachedClasses = 'SideDrawer Close';
+  if (props.open) {
+    attachedClasses = 'SideDrawer Open';
+  }
+  return (
+    <Aux>
+      <Backdrop show={props.open} clicked={props.closed} />
+      <div className={attachedClasses}>
+        <div className="Logo">
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
+      </div>
+    </Aux>
+  );
+};
+
+export default sideDrawer;
