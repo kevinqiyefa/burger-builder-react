470c1ea9504d94f1e86634d2a149601f49eef3e4
Kevin <kevinqiyf@hotmail.com>
2018-11-11 19:32:23 -0800

    displaying current price

--- src/components/Burger/BuildControls/index.js
+++ src/components/Burger/BuildControls/index.js
@@ -11,6 +11,10 @@ const controls = [
 
 const buildControls = props => (
   <div className="BuildControls">
+    <p>
+      Current Price: <strong>${props.price.toFixed(2)}</strong>
+    </p>
+
     {controls.map(ctrl => (
       <BuildControl
         key={ctrl.label}
