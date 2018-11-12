72968f25544b59d867b40a12a3f5ac50f932d19f
Kevin <kevinqiyf@hotmail.com>
2018-11-12 15:30:39 -0800

    adding drawer toggle logic

---
+++ src/components/Navigation/SideDrawer/DrawerToggle/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const drawerToggle = props => (
+  <div onClick={props.clicked}>
+    menu
+    {/* <div></div>
+        <div></div>
+        <div></div> */}
+  </div>
+);
+
+export default drawerToggle;
--- src/components/Navigation/Toolbar/index.js
+++ src/components/Navigation/Toolbar/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import Logo from '../Logo';
 import NavigationItems from '../NavigationItems';
 import './style.css';
+import DrawerToggle from '../SideDrawer/DrawerToggle';
 
 const toolbar = props => (
   <header className="Toolbar">
-    <div>Menu</div>
+    <DrawerToggle clicked={props.drawerToggleClicked} />
     <div className="LogoSection">
       <Logo />
     </div>
