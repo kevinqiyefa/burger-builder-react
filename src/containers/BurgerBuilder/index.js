1d03cd171787d230cc0bc5863a91df8255ce63b1
Kevin <kevinqiyf@hotmail.com>
2018-11-16 17:16:24 -0800

    handling axios error globally by using higher order component

--- src/containers/BurgerBuilder/index.js
+++ src/containers/BurgerBuilder/index.js
@@ -6,6 +6,7 @@ import OrderSummary from '../../components/Burger/OrderSummary';
 import Modal from '../../components/UI/Modal';
 import Spinner from '../../components/UI/Spinner';
 import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -137,4 +138,4 @@ class BurgerBuilder extends Component {
   }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
