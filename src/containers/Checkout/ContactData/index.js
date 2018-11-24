b449cc07104737221520c4881681a7e093bda74e
Kevin <kevinqiyf@hotmail.com>
2018-11-24 13:03:48 -0800

    fixed bug for posting data to backend

--- src/containers/Checkout/ContactData/index.js
+++ src/containers/Checkout/ContactData/index.js
@@ -21,7 +21,7 @@ class ContactData extends Component {
     this.setState({ loading: true });
 
     const order = {
-      ingredients: this.state.ingredients,
+      ingredients: this.props.ingredients,
       price: this.props.price,
       customer: {
         name: 'Kevin Qi',
