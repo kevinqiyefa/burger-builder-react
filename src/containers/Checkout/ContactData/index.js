c8965831e65b066c421c4fef61ff6fcf2b96c1b6
Kevin <kevinqiyf@hotmail.com>
2018-11-23 17:05:22 -0800

    added contact form container

---
+++ src/containers/Checkout/ContactData/index.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+
+import Button from '../../../components/UI/Button';
+import './style.css';
+
+class ContactData extends Component {
+  state = {
+    name: '',
+    email: '',
+    address: {
+      street: '',
+      postalCode: ''
+    },
+    loading: false
+  };
+
+  render() {
+    let form = (
+      <form>
+        <input
+          className="Input"
+          type="text"
+          name="name"
+          placeholder="Your Name"
+        />
+        <input
+          className="Input"
+          type="email"
+          name="email"
+          placeholder="Your Mail"
+        />
+        <input
+          className="Input"
+          type="text"
+          name="street"
+          placeholder="Street"
+        />
+        <input
+          className="Input"
+          type="text"
+          name="postal"
+          placeholder="Postal Code"
+        />
+        <Button btnType="Success">ORDER</Button>
+      </form>
+    );
+
+    return (
+      <div className="ContactData">
+        <h4>Enter your Contact Data</h4>
+        {form}
+      </div>
+    );
+  }
+}
+
+export default ContactData;
--- src/containers/Checkout/index.js
+++ src/containers/Checkout/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary';
+import { Route } from 'react-router-dom';
+import ContactData from './ContactData';
 
 class Checkout extends Component {
   state = {
@@ -37,6 +39,10 @@ class Checkout extends Component {
           ingredients={this.state.ingredients}
           checkoutCancelled={this.checkoutCancelledHandler}
           checkoutContinued={this.checkoutContinuedHandler}
+        />
+        <Route
+          path={this.props.match.path + '/contact-data'}
+          component={ContactData}
         />
       </div>
     );
